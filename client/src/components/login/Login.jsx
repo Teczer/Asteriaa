733748da45400402f35de9ccc022a2cca1d7a748
Mehdi Hattou <mehdi.hattou1@gmail.com>
2023-07-01 19:53:55 +0200

    fix login css + log

--- client/src/components/login/Login.jsx
+++ client/src/components/login/Login.jsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import "./login.scss";
 import { useLogin } from "../../../hooks/useLogin";
+import image from "./launchlogo@3x 2.png";
 
 function Login() {
   const [email, setEmail] = useState("");
@@ -15,66 +16,64 @@ function Login() {
   };
   return (
     <main className="login-screen">
-      <img
-        src="https://res.cloudinary.com/dw3mwclgk/image/upload/v1670710895/launchlogo_3x_gw4cwy.png"
-        alt="Asterialogo"
-        className="asteria-login-fake-logo"
-      />
-      <img
-        src="https://res.cloudinary.com/dw3mwclgk/image/upload/v1670710895/launchlogo_3x_gw4cwy.png"
-        alt="Asterialogo"
-        className="asteria-login-logo"
-      />
-      <h1>
-        Bienvenue sur <span className="asteria-name">Asteria</span>,
-        veuillez-vous authenthifiez
-      </h1>
-      <section className="loginform-container">
-        <div id="login">
-          <form
-            className="login-form" /* onSubmit={this.onSubmit} */
-            onSubmit={handleSubmit}
-          >
-            <span className="fa fa-user" />
-            <input
-              // rome-ignore lint/a11y/noAutofocus: <explanation>
-              autoFocus
-              maxLength="25"
-              /* onChange={this.handleChange.bind(this, "email")} */
-              placeholder="Email"
-              type="email"
-              onChange={(e) => setEmail(e.target.value)}
-              value={email}
-              /* value={this.state.user.email} */
-              required
-            />
-            <span className="fa fa-lock" />
-            <input
-              autoComplete="off"
-              maxLength="12"
-              /* onChange={this.handleChange.bind(this, "password")} */
-              placeholder="Mot de passe"
-              type="password"
-              onChange={(e) => setPassword(e.target.value)}
-              value={password}
-              /* value={this.state.user.password} */
-              required
-            />
-            <input type="submit" value="Se connecter" disabled={isLoading} />
-            {error && (
-              <div className="error" style={{ color: "red" }}>
-                {error}
-              </div>
-            )}
-          </form>
-        </div>
-        <div className="notmember-container">
-          <p>
-            Pas membre ? <a href="/signup">Inscription</a>{" "}
-            <span className="fa fa-arrow-right" />
-          </p>
-        </div>
-      </section>
+      <img className="asteria-login-logo" src={image} alt="Asterialogo" />
+      <div className="login-form-wrapper">
+        <h1 className="login-title">
+          Bienvenue sur <span className="asteria-name">Asteria</span>,
+          veuillez-vous authenthifier
+        </h1>
+        <section className="loginform-container">
+          <div id="login">
+            <form
+              className="login-form" /* onSubmit={this.onSubmit} */
+              onSubmit={handleSubmit}
+            >
+              <span className="fa fa-user" />
+              <input
+                // rome-ignore lint/a11y/noAutofocus: <explanation>
+                autoFocus
+                maxLength="25"
+                /* onChange={this.handleChange.bind(this, "email")} */
+                placeholder="Email"
+                type="email"
+                onChange={(e) => setEmail(e.target.value)}
+                value={email}
+                /* value={this.state.user.email} */
+                required
+              />
+              <span className="fa fa-lock" />
+              <input
+                autoComplete="off"
+                maxLength="12"
+                /* onChange={this.handleChange.bind(this, "password")} */
+                placeholder="Mot de passe"
+                type="password"
+                onChange={(e) => setPassword(e.target.value)}
+                value={password}
+                /* value={this.state.user.password} */
+                required
+              />
+              <input
+                type="submit"
+                value="Se connecter"
+                disabled={isLoading}
+                style={{ marginBottom: "20px" }}
+              />
+              {error && (
+                <div className="error" style={{ color: "red" }}>
+                  {error}
+                </div>
+              )}
+            </form>
+          </div>
+          <div className="notmember-container">
+            <p>
+              Pas membre ? <a href="/signup">Inscription</a>{" "}
+              <span className="fa fa-arrow-right" />
+            </p>
+          </div>
+        </section>
+      </div>
     </main>
   );
 }
