a1d39bb7849f99006b6d0ac5b669e2daae91e8dc
Mehdi Hattou <mehdi.hattou1@gmail.com>
2023-07-01 22:30:31 +0200

    :fire: youpi

--- backend/server.js
+++ backend/server.js
@@ -18,6 +18,17 @@ const app = express();
 // Middleware CORS pour Express
 app.use(cors());
 
+app.use(function (req, res, next) {
+  //Enabling CORS
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, x-client-key, x-client-token, x-client-secret, Authorization"
+  );
+  next();
+});
+
 const server = jsonServer.create();
 const middlewares = jsonServer.defaults();
 
