95ca3792004c30578ce668914c325b08d122e2c4
mehdi_hattou <mehdi_hattou@carrefour.com>
2023-07-24 09:45:40 +0200

    Verification Phase, building back :milky-way: :milky:way:

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,4 +1,8 @@
 import express from "express";
+import jwt from "jsonwebtoken";
+import dotenv from "dotenv";
+
+dotenv.config({ path: "../.env" });
 
 // controller functions
 
@@ -26,3 +30,77 @@ router.patch("/:id", updateUser);
 router.get("/:id", getUser);
 
 export default router;
+
+// VERIFICATION PHASE
+
+// Route pour envoyer l'e-mail de vérification
+router.post("/send-verification-email", async (req, res) => {
+  const { email, userId } = req.body;
+
+  try {
+    const user = await User.findById(userId);
+    if (!user) {
+      return res.status(404).json({ error: "Utilisateur non trouvé" });
+    }
+
+    if (user.email !== email) {
+      return res
+        .status(400)
+        .json({ error: "L'adresse e-mail ne correspond pas à l'utilisateur" });
+    }
+
+    // Générer un jeton de vérification avec une clé secrète
+    const verificationToken = jwt.sign({ userId }, process.env.JWT_SECRET, {
+      expiresIn: "1h", // Durée de validité du token
+    });
+
+    // Enregistrez le jeton dans la base de données pour vérification ultérieure
+    user.verificationToken = verificationToken;
+    await user.save();
+
+    // Envoyez l'e-mail de vérification avec le lien contenant le token
+    await sendVerificationEmail(email, verificationToken);
+
+    res
+      .status(200)
+      .json({ message: "E-mail de vérification envoyé avec succès !" });
+  } catch (error) {
+    console.error(
+      "Erreur lors de l'envoi de l'e-mail de vérification : ",
+      error
+    );
+    res.status(500).json({
+      error:
+        "Une erreur est survenue lors de l'envoi de l'e-mail de vérification",
+    });
+  }
+});
+
+// Route pour traiter la vérification de l'e-mail
+router.get("/verify-email", async (req, res) => {
+  const { token } = req.query;
+
+  try {
+    // Vérifiez le token avec la clé secrète utilisée pour le générer
+    const decodedToken = jwt.verify(token, process.env.JWT_SECRET);
+
+    const user = await User.findOne({ _id: decodedToken.userId });
+    if (!user) {
+      return res
+        .status(404)
+        .json({ error: "Jeton de vérification invalide ou expiré" });
+    }
+
+    // Marquez l'e-mail de l'utilisateur comme vérifié
+    user.isEmailVerified = true;
+    user.verificationToken = undefined; // Effacez le jeton de vérification
+    await user.save();
+
+    res.status(200).json({ message: "E-mail vérifié avec succès !" });
+  } catch (error) {
+    console.error("Erreur lors de la vérification de l'e-mail : ", error);
+    res.status(500).json({
+      error: "Une erreur est survenue lors de la vérification de l'e-mail",
+    });
+  }
+});
---
+++ backend/utils/sendVerificationEmail.js
@@ -0,0 +1,30 @@
+import nodemailer from "nodemailer";
+
+const sendVerificationEmail = async (userEmail, verificationToken) => {
+  const transporter = nodemailer.createTransport({
+    service: "Gmail", // Remplacez par le service de messagerie que vous souhaitez utiliser
+    auth: {
+      user: "asteria.noreply@gmail.com", // Remplacez par votre adresse e-mail d'expéditeur
+      pass: "asteriamail", // Remplacez par le mot de passe de votre adresse e-mail
+    },
+  });
+
+  const mailOptions = {
+    from: "asteria.noreply@gmail.com", // Remplacez par votre adresse e-mail d'expéditeur
+    to: userEmail,
+    subject: "Vérification de votre adresse e-mail",
+    text: `Cliquez sur le lien suivant pour vérifier votre adresse e-mail : http://mehdiv.fr/verify?token=${verificationToken}`,
+  };
+
+  try {
+    await transporter.sendMail(mailOptions);
+    console.log("E-mail de vérification envoyé !");
+  } catch (error) {
+    console.error(
+      "Erreur lors de l'envoi de l'e-mail de vérification : ",
+      error
+    );
+  }
+};
+
+export default sendVerificationEmail;
