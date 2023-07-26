4e9e6cd852f62d37d37add6dd7e41b351b15ca9b
Mehdi Hattou <mehdi.hattou1@gmail.com>
2023-07-26 21:25:32 +0200

    building front deleteUser Modal, :fire: :fire:

--- client/src/components/userSettings/PropertyController.jsx
+++ client/src/components/userSettings/PropertyController.jsx
@@ -2,6 +2,7 @@ import axios from "axios";
 import { useState } from "react";
 import AlertModal from "../header/modal/AlertModal";
 import { useAuthContext } from "../../../hooks/useAuthContext";
+import { Link } from "react-router-dom";
 
 function PropertyController({
   user,
@@ -13,6 +14,8 @@ function PropertyController({
   const [goodResponse, setGoodResponse] = useState(false);
   console.log("errorMsg", errorMsg);
   const [isChanchingUsername, setIsChanchingUsername] = useState(false);
+  const [isTypingPassword, setIsTypingPassword] = useState(false);
+  const [password, setPassword] = useState("");
   const [modal, setModal] = useState(false);
   const [deleteUserModal, setDeleteUserModal] = useState(true);
   const { updateUser } = useAuthContext();
@@ -94,6 +97,16 @@ function PropertyController({
     setUserName(e.target.value);
     // Vérifier si la valeur de l'entrée est différente de la valeur initiale du pseudo
     setIsChanchingUsername(e.target.value !== user.userName);
+  };
+
+  // Fonction pour gérer les modifications dans l'entrée du pseudo
+  const handleTypingPassword = (e) => {
+    setPassword(e.target.value);
+    setIsTypingPassword(true);
+
+    if (e.target.value === "") {
+      setIsTypingPassword(false);
+    }
   };
 
   return (
@@ -206,31 +219,62 @@ function PropertyController({
                   e.stopPropagation();
                 }}
               >
-                <div className="user-label-delete">
-                  Supprimer {user.userName} / {user.email}
+                <div className="delete-modal-title-logo">
+                  <img
+                    className="delete-modal-logo"
+                    src="https://res.cloudinary.com/dw3mwclgk/image/upload/v1670528238/asteriaLogo_af3kfh.svg"
+                    alt="Asteria"
+                  />
+                  <h4 className="delete-modal-title">
+                    Confirmez votre mot de passe
+                  </h4>
                 </div>
-                <div className="big-user-picture-info-delete">
-                  <figure className="figure-user-profile-picture --modal">
+                <div className="delete-modal-profilpicture-username">
+                  <figure className="figure-user-profile-picture --delete-user">
                     <img
                       className="user-profile-picture --modal --property-settings"
                       src={user.profilePicture}
                       alt="user-profil-picture"
                     />
                   </figure>
-                  <span>{user.userName}</span>
-                  <span>{user.email}</span>
+                  <strong className="delete-modal-username">
+                    {user.userName}
+                  </strong>
                 </div>
-                <div className="user-label-delete">
-                  <p>To confirm, type "Votremotdepasse" in the box below</p>
+                <p className="delete-modal-alert-info">
+                  Pour votre sécurité, veuillez entrer votre mot de passe pour
+                  continuer.
+                </p>
+                <div className="delete-modal-label-input">
+                  <label className="delete-modal-label">Mot de passe</label>
                   <input
                     className="input-username --overide"
                     maxLength="16"
-                    placeholder="mot de passe"
-                    type="password"
                     required
+                    type="password"
+                    value={password}
+                    onChange={handleTypingPassword}
                   />
-                  <button className="property-button --dangerous">
-                    Supprimer votre compte
+                </div>
+                <Link className="delete-modal-forgot-password" to="/">
+                  <span>Mot de passe oublié ?</span>
+                </Link>
+                <div className="delete-modal-button-wrapper">
+                  <button
+                    className={`property-button --delete-modal ${
+                      isTypingPassword
+                        ? "--delete-modal-allowed"
+                        : "--delete-modal-validator"
+                    } ${goodResponse ? "--goodResponse" : ""}`}
+                    onClick={() => {
+                      isTypingPassword ? changeUsername() : null;
+                    }}
+                  >
+                    {goodResponse ? (
+                      <i className="fa-solid fa-check" />
+                    ) : (
+                      "Enregistrer les modifications"
+                    )}
                   </button>
                 </div>
               </div>
