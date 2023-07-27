d4b636e8f556a60f1fd8e652a7e1561aaec3b748
Teczer <mehdi.hattou1@gmail.com>
2023-07-27 15:52:10 +0200

    New Header Layout + New Modal :fire: :fire:

--- client/src/components/header/Header.jsx
+++ client/src/components/header/Header.jsx
@@ -1,6 +1,5 @@
 import { Link } from "react-router-dom";
 import "./header.scss";
-import { useLogout } from "../../../hooks/useLogout";
 import { useAuthContext } from "../../../hooks/useAuthContext";
 import { useEffect, useState } from "react";
 import Usermodal from "./modal/Usermodal";
@@ -9,16 +8,12 @@ import ChangePictureModal from "./modal/ChangePictureModal";
 
 const Header = () => {
   const [modalBurger, setModalBurger] = useState(false);
+  const [modalAuthentification, setModalAuthentification] = useState(false);
   const [userModal, setUserModal] = useState(false);
   const [isChangingProfilePicture, setIsChangingProfilePicture] =
     useState(false);
 
-  const { logout } = useLogout();
   const { user } = useAuthContext();
-
-  const handleClick = () => {
-    logout();
-  };
 
   useEffect(() => {
     console.log("userFROMHEADER", user);
@@ -42,7 +37,10 @@ const Header = () => {
             fill="#FFFFFF"
             xmlns="http://www.w3.org/2000/svg"
             className="burger-svg"
-            onClick={() => setModalBurger(true)}
+            onClick={() => {
+              setModalAuthentification(false);
+              setModalBurger(true);
+            }}
           >
             <path
               d="M1.94631 16C0.871392 16 0 15.1286 0 14.0537C0 14.024 0.0240384 14 0.0536913 14L20.0537 14C21.1286 14 22 14.8714 22 15.9463C22 15.976 21.976 16 21.9463 16H1.94631Z"
@@ -79,9 +77,15 @@ const Header = () => {
           </div>
         )}
         <div className="nav-menu">
-          <Link to="/">Accueil</Link>
-          <Link to="quizz">Quizz</Link>
-          <Link to="news">Actualités</Link>
+          <Link className="link-nav-wrapper" to="/">
+            Accueil
+          </Link>
+          <Link className="link-nav-wrapper" to="quizz">
+            Quizz
+          </Link>
+          <Link className="link-nav-wrapper" to="news">
+            Actualités
+          </Link>
         </div>
         {user && (
           <div className="logout-wrapper">
@@ -106,15 +110,81 @@ const Header = () => {
 
         {!user && (
           <div className="authentification-wrapper">
-            <Link className="signup" to="login">
+            <Link
+              className="modal-user-notconnected-connexion --desktop"
+              to="login"
+            >
               Connexion
             </Link>
-            <Link to="signup">
-              <input type="submit" value="INSCRIPTION" />
+            <Link
+              className="modal-user-notconnected-inscription --desktop"
+              to="signup"
+            >
+              Inscription
             </Link>
           </div>
         )}
+
+        {!user && (
+          <>
+            {modalAuthentification ? (
+              <button className="menu-burger">
+                <i
+                  className="fa-solid fa-xmark"
+                  onClick={() => setModalAuthentification(false)}
+                />
+              </button>
+            ) : (
+              <svg
+                role="img"
+                aria-hidden="true"
+                width="40"
+                height="40"
+                viewBox="0 0 22 24"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+                className={`burger-svg ${modalBurger ? "--hidden" : ""}`}
+                onClick={() => setModalAuthentification(true)}
+              >
+                <path
+                  d="M6.10527 13.2415C6.62693 13.7184 7.2185 14.1202 7.86282 14.4297C7.83316 14.4351 7.80351 14.4406 7.77386 14.4461L6.83118 14.8182C4.77735 15.923 3.27451 18.0633 2.34357 21.3578C2.30639 21.4894 2.38291 21.6262 2.51449 21.6634C2.5364 21.6696 2.55905 21.6727 2.58181 21.6727L19.4199 21.6727C19.5566 21.6727 19.6674 21.5618 19.6674 21.4251C19.6674 21.4023 19.6643 21.3797 19.6581 21.3578C18.7274 18.0642 17.2252 15.9243 15.1723 14.8192L14.191 14.4348C14.1759 14.432 14.1607 14.4292 14.1456 14.4265C14.7873 14.1174 15.3766 13.7168 15.8964 13.2415C18.4531 14.5762 20.2629 17.1246 21.3259 20.8865C21.3755 21.0617 21.4006 21.243 21.4006 21.4251C21.4006 22.519 20.5138 23.4058 19.4199 23.4058L2.58181 23.4058C2.3997 23.4058 2.21845 23.3807 2.0432 23.3312C0.990498 23.0337 0.378263 21.9392 0.675732 20.8865C1.73875 17.1246 3.5486 14.5762 6.10527 13.2415V13.2415Z"
+                  fill="#FFFFFF"
+                ></path>
+                <path
+                  fill-rule="evenodd"
+                  clip-rule="evenodd"
+                  d="M18.2582 7.88763C18.2582 11.8956 15.0091 15.1446 11.0012 15.1446C6.99321 15.1446 3.74414 11.8956 3.74414 7.88763C3.74414 3.87969 6.99321 0.630615 11.0012 0.630615C15.0091 0.630615 18.2582 3.87969 18.2582 7.88763ZM5.47734 7.88508C5.47734 10.9358 7.95046 13.4089 11.0012 13.4089C14.052 13.4089 16.5251 10.9358 16.5251 7.88508C16.5251 4.83433 14.052 2.36121 11.0012 2.36121C7.95046 2.36121 5.47734 4.83433 5.47734 7.88508Z"
+                  fill="#FFFFFF"
+                ></path>
+              </svg>
+            )}
+          </>
+        )}
       </header>
+      {modalAuthentification && (
+        <div className="modal-container --user animated">
+          <div className="modal-container --layoutuser animated">
+            <div className="modal-user-notconnected-infos">
+              <p className="modal-user-notconnected-title">
+                Bienvenue chez Asteria
+              </p>
+              <p className="modal-user-notconnected-info">
+                Connectez-vous ou inscrivez-vous pour profiter pleinement de
+                l'expérience Asteria !
+              </p>
+            </div>
+            <div className="modal-user-notconnected-buttons">
+              <button className="modal-user-notconnected-inscription">
+                Inscription
+              </button>
+              <button className="modal-user-notconnected-connexion">
+                Connexion
+              </button>
+            </div>
+          </div>
+        </div>
+      )}
+
       {modalBurger && <Modalburger setModalBurger={setModalBurger} />}
     </>
   );
