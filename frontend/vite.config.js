473adfab98dc11109fbe73d027698225bc0dbe61
Teczer <mehdi.hattou1@gmail.com>
2023-12-14 00:12:14 +0100

    :card_index: PWA : Adding Progressiv Web App hein

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,48 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-
+import { VitePWA } from "vite-plugin-pwa";
+const manifestForPlugIn = {
+  registerType: "prompt",
+  includeAssests: ["favicon.ico", "apple-touc-icon.png", "masked-icon.svg"],
+  manifest: {
+    name: "Asteria Space",
+    short_name: "Asteria",
+    description: "I am a Space app",
+    icons: [
+      {
+        src: "./public/android-chrome-192x192.png",
+        sizes: "192x192",
+        type: "image/png",
+        purpose: "favicon",
+      },
+      {
+        src: "./public/android-chrome-512x512.png",
+        sizes: "512x512",
+        type: "image/png",
+        purpose: "favicon",
+      },
+      {
+        src: "./public/apple-touch-icon.png",
+        sizes: "180x180",
+        type: "image/png",
+        purpose: "apple touch icon",
+      },
+      {
+        src: "./public/maskable-icon.png",
+        sizes: "512x512",
+        type: "image/png",
+        purpose: "any maskable",
+      },
+    ],
+    theme_color: "#171717",
+    background_color: "#f0e7db",
+    display: "standalone",
+    scope: "/",
+    start_url: "/",
+    orientation: "portrait",
+  },
+};
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), VitePWA(manifestForPlugIn)],
 });
